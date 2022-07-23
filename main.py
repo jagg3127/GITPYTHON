14aef7b415fc23b7c89e12492bcb03a5b61704dd
u0_a384 <u0_a384@localhost>
2022-07-22 22:18:29 -0400

    python

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from git import Repo
 import os
 repo = Repo.init(os.getcwd())
-repo.index.add([os.getcwd()+"/*"])
-repo.index.commit("initial commit")
-#repo.create_remote("origin", url="https://github.com/jagg3127/GITPYTHON.git")
-print(repo.branches)
-#repo.remote("origin").push()
+repo.git.add(update=True)
+repo.index.commit("python")
+origin = repo.remote(name='origin', url='https://github.com/jagg3127/GITPYTHON')
+origin.push()
